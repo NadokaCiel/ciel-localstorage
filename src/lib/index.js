95d03c6cdbd474f053a478143fdd13416e43a43e
Ciel <ciel.zhang@verystar.cn>
2019-11-26 16:37:15 +0800

    fix: 修复base64加密错误

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,5 +1,6 @@
 import SHA1 from 'crypto-js/sha1';
 
+import Utf8 from 'crypto-js/enc-utf8';
 import Base64 from 'crypto-js/enc-base64';
 
 const sha1 = (string) => SHA1(string).toString();
@@ -11,9 +12,13 @@ export function setLocalStorage(key, value, expired) {
   if (!key && key !== 0) {
     return '';
   }
-  window.localStorage.setItem(sha1(key), Base64.stringify(JSON.stringify(value)));
+  // console.log('value', value);
+  // console.log(`JSON.stringify`, JSON.stringify(value + ''));
+  // console.log(`Utf8.parse`, Utf8.parse(JSON.stringify(value + '')));
+  // console.log(`Base64.stringify`, Base64.stringify(Utf8.parse(JSON.stringify(value + ''))));
+  window.localStorage.setItem(sha1(key), Base64.stringify(Utf8.parse(JSON.stringify(value + ''))));
   if (expired && Number.isInteger(expired)) {
-    window.localStorage.setItem(sha1(`${key}__expires__`), Base64.stringify(Date.now() + 1000 * expired));
+    window.localStorage.setItem(sha1(`${key}__expires__`), Base64.stringify(Utf8.parse(Date.now() + 1000 * expired)));
   }
   return value;
 }
@@ -23,7 +28,7 @@ export function getLocalStorage(key) {
   if (!time) {
     return '';
   }
-  const expired = Base64.parse(time);
+  const expired = Utf8.stringify(Base64.parse(time));
   const now = Date.now();
 
   if (expired && now >= parseInt(expired, 10)) {
@@ -34,7 +39,11 @@ export function getLocalStorage(key) {
   if (!data) {
     return '';
   }
-  const value = Base64.parse(data);
+  // console.log('data', data);
+  // console.log('Base64.parse', Base64.parse(data));
+  // console.log('Utf8.stringify', Utf8.stringify(Base64.parse(data)));
+  // console.log('value', JSON.parse(Utf8.stringify(Base64.parse(data))));
+  const value = Utf8.stringify(Base64.parse(data));
   return value ? JSON.parse(value) : null;
 }
 
